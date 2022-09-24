2b3c845624ac0bf34bed2becfdd0d0e769544cf0
router <messagebus@vk.com>
2022-09-24 03:11:29 +0300

    how it feels to not update arch for 2 days

--- Content.Client/Links/UILinks.cs
+++ Content.Client/Links/UILinks.cs
@@ -2,10 +2,10 @@
 {
     public static class UILinks
     {
-        public const string GitHub = "https://github.com/space-wizards/space-station-14/";
+        public const string GitHub = "https://github.com/BasedUser/ss14-routers-den/";
         public const string Patreon = "https://www.patreon.com/spacestation14";
 
-        public const string Discord = "https://discordapp.com/invite/t2jac3p";
+        public const string Discord = "https://discordapp.com/invite/DdF3CPNrxJ";
         public const string BugReport = "https://github.com/space-wizards/space-station-14/issues/new/choose";
         public const string Website = "https://spacestation14.io";
         public const string Wiki = "https://wiki.spacestation14.io";
